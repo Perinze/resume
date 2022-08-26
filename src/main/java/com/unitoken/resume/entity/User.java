68c130f4f32b44fecfcaf5f6be565302e7049e84
perinze <perinzefaper@foxmail.com>
2022-08-27 00:46:19 +0800

    add class User

---
+++ src/main/java/com/unitoken/resume/entity/User.java
@@ -0,0 +1,57 @@
+package com.unitoken.resume.entity;
+
+public class User {
+
+    private Long id;
+
+    private String nick;
+    private String openid;
+    private Department department;
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User[%s, %s, %s, %s, %s]",
+                getId(), getNick(), getOpenid(), getDepartment(), getRole());
+    }
+}
